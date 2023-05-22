ba9948b7a9d4cf2e23f36c39c93d824615f461bb
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-22 11:19:34 +0800

    Add author list page and update MVC

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -27,6 +28,27 @@ AuthorSchema.virtual('name').get(function () {
 AuthorSchema.virtual('url').get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('lifespan').get(function () {
+  const birthDate = this.date_of_birth;
+  const deathDate = this.date_of_death;
+
+  if (birthDate && deathDate) {
+    return (
+      DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+        DateTime.DATE_MED,
+      ) +
+      ' - ' +
+      DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    );
+  } else if (birthDate) {
+    return (
+      DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+        DateTime.DATE_MED,
+      ) + ' - present'
+    );
+  } else return '';
 });
 
 // Export model
